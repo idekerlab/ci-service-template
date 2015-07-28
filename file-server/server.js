634a6bbc1f7919b7e7cbf1a0ee4cecac15c40f08
Keiichiro Ono <keiono@gmail.com>
2015-07-27 18:31:07 -0700

    Simple file server has been implemented with node.js.

---
+++ file-server/server.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var fs = require("fs");
+
+var PORT = 3000;
+
+var DATA_DIR = '/data';
+var OUT_DIR = '/out';
+
+var app = express();
+
+var counter = 0;
+
+/**
+For streaming data from file
+*/
+app.get('/test', function (req, res) {
+	var stream = fs.createReadStream(__dirname + DATA_DIR + '/test.txt');
+    stream.pipe(res);
+});
+
+app.get('/data/:id', function (req, res) {
+	var fileId = req.params.id;
+	var stream = fs.createReadStream(__dirname + DATA_DIR + '/test_' + fileId + '.json');
+    stream.pipe(res);
+});
+
+/**
+For streaming POST request doby into file
+*/
+app.post('/data', function(req, res) {
+	var writeStream = fs.createWriteStream(__dirname + DATA_DIR + '/test_' + (counter++) + '.json');
+
+  	// This pipes the POST data to the file
+  	req.pipe(writeStream);
+  	req.on('end', function () {
+	  	var message = {
+	  		message: 'success'
+	  	}
+	  	res.send(message);
+  	});
+
+});
+
+
+
+var server = app.listen(PORT, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("File Server is listening at http://%s:%s", host, port);
+
+});
