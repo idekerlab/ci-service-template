c6eaddd326eabb58388e9b3a42a6a3eb8aa084b5
Keiichiro Ono <keiono@gmail.com>
2015-09-29 17:23:24 -0700

    Basic deletion added to task API.

--- ciservice/apiv1/queue/task.py
+++ ciservice/apiv1/queue/task.py
@@ -3,8 +3,11 @@ import logging
 
 from flask.ext.restful import Resource
 import redis
+import requests
 
 from . import TAG_JOB_ID, TAG_STATUS
+
+INPUT_DATA_SERVER_LOCATION = 'http://dataserver:3000/'
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -28,5 +31,32 @@ class Task(Resource):
         result = {
             TAG_JOB_ID: job_id,
             TAG_STATUS: status
+        }
+        return result, 200
+
+    def delete(self, job_id):
+        """
+        Delete both job and its result.
+        :return: Success message
+        """
+        logging.debug('Got deletion request for: ' + str(job_id))
+
+        # Delete both input and result cache files
+        result_location = self.__redis_connection.hget(name='results', key=job_id)
+        logging.debug('Deleting result file: ' + str(result_location))
+
+        # Result file
+        req = requests.delete(result_location)
+        logging.debug(str(req.json()))
+
+        # Delete from status list
+        self.__redis_connection.hdel(TAG_STATUS, (job_id))
+
+        # TODO: send status of job to monitor
+        # self.__monitor.send_json(current_status)
+
+        result = {
+            TAG_JOB_ID: job_id,
+            TAG_STATUS: 'deleted'
         }
         return result, 200
